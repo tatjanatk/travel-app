41454bb5416e857624316a2d7a8e4eebe7119a47
Tatjana <Tatjana.aha@telekom.de>
2022-12-20 16:47:46 +0100

    style changes

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,11 @@ import { handleSubmit } from "./js/formHandler";
 import { updateUI } from "./js/formHandler";
 import { postData } from "./js/formHandler";
 
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+
 export {
     handleSubmit,
     updateUI,
